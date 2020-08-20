9fbefc07d4b3e071a221a2be8328a01ab9c4ef97
Brandon Vergara <brandon@makingdevs.com>
2020-08-19 23:59:05 -0500

    coding master  creating project in mix

---
+++ lib/kata_resuelve.ex
@@ -0,0 +1,18 @@
+defmodule KataResuelve do
+  @moduledoc """
+  Documentation for `KataResuelve`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KataResuelve.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kata_resuelve_test.exs
@@ -0,0 +1,8 @@
+defmodule KataResuelveTest do
+  use ExUnit.Case
+  doctest KataResuelve
+
+  test "greets the world" do
+    assert KataResuelve.hello() == :world
+  end
+end
