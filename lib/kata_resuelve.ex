be0ca39a7d9ecf3ff13a1506a90f3a59e0a7dafe
Brandon Vergara <brandon@makingdevs.com>
2020-08-20 00:32:01 -0500

    coding master  add function to read json from file

--- lib/kata_resuelve.ex
+++ lib/kata_resuelve.ex
@@ -3,20 +3,11 @@ defmodule KataResuelve do
   Documentation for `KataResuelve`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-      iex> path = './file.json'
-      iex> KataResuelve.parser_map_from_json_file(path)
-      %{}
-
-  """
   def parser_map_from_json_file(path) do
     case File.read(path) do
-      {:ok, file} -> IO.inspect file
+      {:ok, file} ->
+        Jason.decode!(file)
       {:error, _} -> IO.puts "Error to read file"
     end
-    %{}
   end
 end
--- test/kata_resuelve_test.exs
+++ test/kata_resuelve_test.exs
@@ -4,6 +4,7 @@ defmodule KataResuelveTest do
 
   test "get map from json file" do
     path = './jugadores.json'
-    assert KataResuelve.parser_map_from_json_file(path) == %{}
+    map_from_json = KataResuelve.parser_map_from_json_file(path)
+    assert List.first(map_from_json["jugadores"]) == %{"bono" => 25000, "equipo" => "rojo", "goles" => 10, "nivel" => "C", "nombre" => "Juan Perez", "sueldo" => 50000, "sueldo_completo" => nil}
   end
 end
