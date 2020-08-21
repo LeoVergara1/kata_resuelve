5568757ef43129ca71c1de3e31822b2e2f285361
Brandon Vergara <brandon@makingdevs.com>
2020-08-21 00:49:08 -0500

    coding master  add test to calculate percentage by team

--- lib/kata_resuelve.ex
+++ lib/kata_resuelve.ex
@@ -2,6 +2,7 @@ defmodule KataResuelve do
   @moduledoc """
   Documentation for `KataResuelve`.
   """
+  @goals_by_nivel %{"A" => 5, "B" => 10, "C" => 15, "Cuauh" => 20}
 
   def parser_map_from_json_file(path) do
     case File.read(path) do
@@ -9,5 +10,18 @@ defmodule KataResuelve do
         Jason.decode!(file)
       {:error, _} -> IO.puts "Error to read file"
     end
+  end
+
+  def calculate_percentage_by_team(list_players) do
+    teams = Enum.group_by(list_players, fn (x) -> x["equipo"] end)
+    for {key, value} <- teams, into: %{} do
+      goal_players_by_team = Enum.map(value, fn(x) -> x["goles"] end)
+      goal_require_by_team = Enum.map(value, fn(x) -> @goals_by_nivel[x["nivel"]] end)
+      percentage = Enum.sum(goal_players_by_team) / Enum.sum(goal_require_by_team) * 100
+      {key, percentage}
+    end
+  end
+
+  def calculate_total_salary do
   end
 end
--- test/kata_resuelve_test.exs
+++ test/kata_resuelve_test.exs
@@ -7,4 +7,41 @@ defmodule KataResuelveTest do
     map_from_json = KataResuelve.parser_map_from_json_file(path)
     assert List.first(map_from_json["jugadores"]) == %{"bono" => 25000, "equipo" => "rojo", "goles" => 10, "nivel" => "C", "nombre" => "Juan Perez", "sueldo" => 50000, "sueldo_completo" => nil}
   end
+
+  test "calculate percentage by team" do
+    player = [
+      %{
+      "nombre" => "Juan",
+      "nivel" => "A",
+      "goles" => 6,
+      "sueldo" => 50000,
+      "bono" => 25000,
+      "sueldo_completo" => nil,
+      "equipo" => "rojo"
+      },
+      %{
+      "nombre" => "Pedro",
+      "nivel" => "B",
+      "goles" => 7,
+      "sueldo" => 60000,
+      "bono" => 35000,
+      "sueldo_completo" => nil,
+      "equipo" => "azul"
+      },
+      %{
+      "nombre" => "Martin",
+      "nivel" => "B",
+      "goles" => 16,
+      "sueldo" => 40000,
+      "bono" => 35000,
+      "sueldo_completo" => nil,
+      "equipo" => "rojo"
+      },
+    ]
+    assert KataResuelve.calculate_percentage_by_team(player) == %{"azul" => 70.0, "rojo" => 146.66666666666666} ## Duda!!! Que pasa si el equipo anota más goles de los requeridos!! Se toma como extra?
+  end
+
+  test "calculate total salary" do
+    
+  end
 end
